503379f184a1b1d9569ad1c88121ffdf6f8a71f7
akholodok <akholodok@gmail.com>
2018-06-26 19:39:08 +0300

    application setup.

---
+++ src/main/java/com/akholodok/stats/aggregator/StatsAggregatorApplication.java
@@ -0,0 +1,12 @@
+package com.akholodok.stats.aggregator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StatsAggregatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StatsAggregatorApplication.class, args);
+	}
+}
