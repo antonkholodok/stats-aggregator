e11a169d607723d525011a101e4e6ac7474ccb7a
akholodok <akholodok@gmail.com>
2018-06-26 19:42:26 +0300

    added tests

---
+++ src/test/java/com/akholodok/stats/aggregator/controller/TransactionsControllerTests.java
@@ -0,0 +1,112 @@
+package com.akholodok.stats.aggregator.controller;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+
+import com.akholodok.stats.aggregator.conf.AppConfiguration;
+import com.akholodok.stats.aggregator.model.Stats;
+import com.akholodok.stats.aggregator.service.TimeSource;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.test.annotation.DirtiesContext;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+
+@RunWith(SpringRunner.class)
+@WebAppConfiguration
+@ContextConfiguration(classes = AppConfiguration.class)
+@DirtiesContext(classMode = DirtiesContext.ClassMode.AFTER_EACH_TEST_METHOD)
+public class TransactionsControllerTests {
+
+    private static final ObjectMapper mapper = new ObjectMapper();
+
+    private MockMvc mockMvc;
+
+    @Autowired
+    private WebApplicationContext webApplicationContext;
+    @Autowired
+    private TimeSource timeSource;
+
+    @Before
+    public void setup() {
+        this.mockMvc = MockMvcBuilders.webAppContextSetup(webApplicationContext).build();
+    }
+
+    @Test(timeout = 5_000)
+    public void testGetStatsEmpty() throws Exception {
+        this.mockMvc.perform(get(TransactionsController.PATH)
+            .accept(MediaType.parseMediaType(MediaType.APPLICATION_JSON_UTF8_VALUE)))
+            .andExpect(status().isNoContent());
+    }
+
+    @Test(timeout = 5_000)
+    public void testAddStatsAndGetStats() throws Exception {
+
+        double amount = 10.0;
+        PostTransaction request = new PostTransaction(amount, timeSource.now().toEpochMilli());
+
+        this.mockMvc.perform(
+            post(TransactionsController.PATH)
+                .content(mapper.writeValueAsBytes(request))
+                .contentType(MediaType.parseMediaType(MediaType.APPLICATION_JSON_UTF8_VALUE))
+                .accept(MediaType.APPLICATION_JSON_UTF8_VALUE))
+            .andExpect(status().isOk());
+
+        Stats expectedResponse = new Stats(1, amount, amount, amount);
+        this.mockMvc.perform(
+            get(TransactionsController.PATH)
+                .contentType(MediaType.parseMediaType(MediaType.APPLICATION_JSON_UTF8_VALUE))
+                .accept(MediaType.APPLICATION_JSON_UTF8_VALUE))
+            .andExpect(status().isOk())
+            .andExpect(content().contentType(MediaType.APPLICATION_JSON_UTF8_VALUE))
+            .andExpect(content().json(mapper.writeValueAsString(expectedResponse)));
+    }
+
+    @Test(timeout = 5_000)
+    public void testAddStatsInFuture() throws Exception {
+
+        double amount = 10.0;
+        PostTransaction request = new PostTransaction(amount, timeSource.now().plusSeconds(10).toEpochMilli());
+
+        this.mockMvc.perform(
+            post(TransactionsController.PATH)
+                .content(mapper.writeValueAsBytes(request))
+                .contentType(MediaType.parseMediaType(MediaType.APPLICATION_JSON_UTF8_VALUE))
+                .accept(MediaType.APPLICATION_JSON_UTF8_VALUE))
+            .andExpect(status().isNoContent());
+    }
+
+    @Test(timeout = 5_000)
+    public void testAddStatsMissingAmount() throws Exception {
+        PostTransaction request = new PostTransaction(null, timeSource.now().plusSeconds(10).toEpochMilli());
+        this.mockMvc.perform(
+            post(TransactionsController.PATH)
+                .content(mapper.writeValueAsBytes(request))
+                .contentType(MediaType.parseMediaType(MediaType.APPLICATION_JSON_UTF8_VALUE))
+                .accept(MediaType.APPLICATION_JSON_UTF8_VALUE))
+            .andExpect(status().isBadRequest());
+        //.andExpect(content().contentType(MediaType.APPLICATION_JSON_UTF8_VALUE));
+    }
+
+    @Test(timeout = 5_000)
+    public void testAddStatsMissingTimestamp() throws Exception {
+        PostTransaction request = new PostTransaction(10.0, null);
+        this.mockMvc.perform(
+            post(TransactionsController.PATH)
+                .content(mapper.writeValueAsBytes(request))
+                .contentType(MediaType.parseMediaType(MediaType.APPLICATION_JSON_UTF8_VALUE))
+                .accept(MediaType.APPLICATION_JSON_UTF8_VALUE))
+            .andExpect(status().isBadRequest());
+        //.andExpect(content().contentType(MediaType.APPLICATION_JSON_UTF8_VALUE));
+    }
+}
---
+++ src/test/java/com/akholodok/stats/aggregator/service/BucketStatsAggregatorTests.java
@@ -0,0 +1,132 @@
+package com.akholodok.stats.aggregator.service;
+
+import static junit.framework.TestCase.*;
+import static org.mockito.Mockito.*;
+
+import com.akholodok.stats.aggregator.model.Stats;
+import junit.framework.TestCase;
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.Mockito;
+
+import java.time.Instant;
+import java.util.List;
+import java.util.Optional;
+import java.util.concurrent.BrokenBarrierException;
+import java.util.concurrent.CyclicBarrier;
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
+public class BucketStatsAggregatorTests {
+
+    private static final int SECONDS = 60;
+
+    private StatsAggregator aggregator;
+    private TimeSource timeSource;
+
+    @Before
+    public void setUp() {
+        timeSource = mock(TimeSource.class);
+        aggregator = new BucketStatsAggregator(SECONDS, timeSource);
+    }
+
+    @Test
+    public void testDefaultBucketReducer() {
+        long epochSeconds = Instant.now().getEpochSecond();
+        assertEquals(
+            new BucketStatsAggregator.Bucket(epochSeconds, 5, 1.0, 13.0, 30.0),
+            BucketStatsAggregator.DEFAULT_BUCKET_REDUCER.apply(
+                new BucketStatsAggregator.Bucket(epochSeconds, 2, 1.0, 10.0, 10.0),
+                new BucketStatsAggregator.Bucket(epochSeconds, 3, 2.0, 13.0, 20.0)));
+    }
+
+    @Test(timeout = 5_000)
+    public void testConcurrentScenario() {
+
+        //
+        // Run N threads in parallel each performing K aggregator inserts rounds.
+        // Each round is represented by one second and performs [1..M] inserts.
+        //
+        // So the total expected stats:
+        // stats = {
+        //      count = N * K * M
+        //      min   = 1
+        //      max   = M
+        //      sum   = N * K * (M * (M + 1) / 2)
+        // }
+        //
+
+        Instant now = Instant.now();
+        when(timeSource.now()).thenReturn(now);
+
+        int threadsCount = Math.max(Runtime.getRuntime().availableProcessors(), 2);
+        int inserts = 30;
+        int rounds = SECONDS;
+        AtomicInteger roundsCompleted = new AtomicInteger();
+
+        CyclicBarrier barrier = new CyclicBarrier(
+            threadsCount,
+            () -> when(timeSource.now()).thenReturn(now.plusSeconds(roundsCompleted.incrementAndGet())));
+
+        List<Thread> threads = IntStream.range(0, threadsCount)
+            .mapToObj(i -> new Thread(new SeedRunnable(aggregator, timeSource, barrier, rounds, inserts)))
+            .peek(Thread::start)
+            .collect(Collectors.toList());
+
+        for (Thread thread : threads) {
+            try {
+                thread.join();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+
+        //
+        when(timeSource.now()).thenReturn(now.plusSeconds(roundsCompleted.get() - 1));
+        Optional<Stats> statsOpt = aggregator.getStats();
+        assertTrue(statsOpt.isPresent());
+
+        Stats stats = statsOpt.get();
+        assertEquals(1.0, stats.getMin());
+        assertEquals((double) inserts, stats.getMax());
+        assertEquals(threadsCount * rounds * inserts, stats.getCount());
+        assertEquals((double) (threadsCount * rounds * inserts * (inserts + 1) / 2), stats.getSum());
+    }
+
+    class SeedRunnable implements Runnable {
+
+        private final StatsAggregator aggregator;
+        private final TimeSource timeSource;
+
+        private final int rounds;
+        private final int inserts;
+
+        private CyclicBarrier barrier;
+
+        public SeedRunnable(StatsAggregator aggregator,
+                            TimeSource timeSource,
+                            CyclicBarrier barrier,
+                            int rounds, int inserts) {
+            this.aggregator = aggregator;
+            this.timeSource = timeSource;
+            this.barrier = barrier;
+            this.rounds = rounds;
+            this.inserts = inserts;
+        }
+
+        @Override
+        public void run() {
+            for (int round = 0; round < rounds; ++round) {
+                IntStream.range(1, inserts + 1).forEach(i -> aggregator.add(timeSource.now(), i));
+                try {
+                    barrier.await();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                } catch (BrokenBarrierException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+}
